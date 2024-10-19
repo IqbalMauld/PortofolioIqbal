96635cf07ff0cf88728fdffeeb96937c803b15e6
IqbalMauld <140826458+IqbalMauld@users.noreply.github.com>
2024-10-19 22:31:35 +0700

    Update script.js

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,6 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const extra = document.querySelector(".extra");
+
+  extra.forEach((item, index) => {
+      gsap.to(item, {
+          opacity: 1,
+          x: 0,
+          duration: 2,
+          ease: "power2.out"
+      });
+  });
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const navLogo = document.querySelectorAll(".nav-logo");
+
+  navLogo.forEach((item, index) => {
+      gsap.to(item, {
+          opacity: 1,
+          x: 0,
+          duration: 2,
+          ease: "power2.out"
+      });
+  });
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const navItems = document.querySelectorAll(".nav a");
+
+  navItems.forEach((item, index) => {
+      gsap.to(item, {
+          opacity: 1,
+          x: 0,
+          duration: 1.5,
+          delay: index * 0.8, // Stagger the animation
+          ease: "power2.out"
+      });
+  });
+});
+gsap.registerPlugin(TextPlugin);
+    
+gsap.to('.text', {duration: 1.5, delay: 2, text: 'Web Developer'});
+// gsap.from('header', {duration: 1.5, y: -100, opacity: 0, ease: 'bounce'}); 
+gsap.from('.left', {duration: 1.5, delay: 1, x: -100, opacity: 0});
+gsap.from('.right', {duration: 1.5, delay: 1, x: 100, opacity: 0});
+gsap.from('.about', {duration: 1.5, delay: 1, x: 100, opacity: 0});
+
 const nav = document.querySelector('.nav');
 const menu = document.querySelector('.extra');
-
 menu.addEventListener('click', () =>{
     nav.classList.toggle('active');
 
